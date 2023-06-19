837957109b06846eace54e82a840eb479e1945cc
Valentin <longovalentin@gmail.com>
2023-06-19 19:48:23 -0300

    CORS

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import indexRouter from "./src/routes/index.route.js"
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: '*', // Permitir todas las solicitudes
+  methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
+  allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version', // Encabezados permitidos
+  credentials: true // Permitir envío de cookies y credenciales
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
